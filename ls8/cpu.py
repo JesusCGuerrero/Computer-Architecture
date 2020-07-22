b6f6cfee34e3e8d221d3d58834a78e74a2554c5e
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-07-22 14:32:46 -0400

    mvp day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,12 +9,15 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.running = False
-        self.LDI = 0b10000010 # set the value of a register to an integer (130)
-        self.PRN = 0b01000111 # Print numeric value stored in the given register (71)
+        self.LDI = 0b10000010 # Set the value of a register to an integer
+        self.PRN = 0b01000111 # Print numeric value stored in the given register
         self.HLT = 0b00000001 # Halt the CPU (and exit the emulator) (1)
         self.MUL = 0b10100010 # MUL
-        self.PUSH = 0b01000101 # PUSH R0
-        self.POP = 0b01000110 # POP R2
+        self.PUSH = 0b01000101 # PUSH
+        self.POP = 0b01000110 # POP
+        self.SUB = False
+        self.DIV = False
+        self.reg[7] = 0xF4
         
     def load(self):
         """Load a program into memory."""
@@ -37,47 +40,6 @@ class CPU:
             print(f"Error {sys.argv[1]}")
             sys.exit(1)
 
-    # def load(self):
-    #     """Load a program into memory."""
-    #     address = 0
-    #     # For now, we've just hardcoded a program:
-    #     # program = [
-    #     #     # From print8.ls8
-    #     #     0b10000010, # 130 LDI
-    #     #     0b00000000, # 0
-    #     #     0b00001000, # 8
-    #     #     0b01000111, # 71 PRN
-    #     #     0b00000000, # 0
-    #     #     0b00000001, # 1 HLT
-    #     # ]
-
-    #     # for instruction in program:
-    #     #     self.ram[address] = instruction
-    #     #     address += 1
-    #     if len(sys.argv) != 2:
-    #         print("usage: ls8.py filename")
-    #         sys.exit(1)
-
-    #     try:
-    #         with open(sys.argv[1]) as f:
-    #             for line in f:
-    #                 # try:
-    #                 line = line.split("#", 1)[0]
-    #                 if line == "" or '\n':
-    #                     continue
-    #                 line = int(line, 2)
-    #                 self.ram[address] = line
-    #                 address += 1
-    #                 # except ValueError:
-    #                 #     print(f"Could not find file {sys.argv[1]}")
-    #                 #     sys.exit(1)
-    #                 print("RAM", self.ram)
-    #     except FileNotFoundError:
-    #         print(f"Error {sys.argv[1]}")
-    #         sys.exit(1)
-
-    # mar stands for memory address register
-    # mdr stands for memory data register
     def ram_read(self, mar):
         return self.ram[mar]
 
@@ -104,8 +66,6 @@ class CPU:
         """
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -119,17 +79,9 @@ class CPU:
         """Run the CPU."""
         running = True # set CPU to running
         while running:
-            # instruction register = ram[self.pc]
-            # Setting ir to be whichever program we are currently running from ram
             ir = self.ram[self.pc]
-            # print(ir) # 130
-            # print(self.LDI) # 130
-            # print('before reg_a')
             reg_a = self.ram_read(self.pc + 1)
-            # print('before reg_b')
             reg_b = self.ram_read(self.pc + 2)
-
-            # if instruction register(ir) == load immediate (LDI)
 
             if ir == self.LDI:
                 # create a variable(register_number) from ram at address 1
@@ -138,27 +90,60 @@ class CPU:
                 value = self.ram[self.pc+2] # Set value to 8
                 # access our reg, at our register_number index(1) and set it to value
                 self.reg[register_number] = value
-                # print(self.reg)
+                # Increment program counter enough to start the next program
                 self.pc += 3
-                # print(f'REGISTER NUMBER: {register_number} VALUE: {value}')
+
             elif ir == self.MUL:
                 self.alu("MUL", reg_a, reg_b)
+                # Increment program counter enough to start the next program
                 self.pc += 3
+
+            elif ir == self.SUB:
+                self.alu("SUB", reg_a, reg_b)
+                # Increment program counter enough to start the next program
+                self.pc += 3
+
+            elif ir == self.DIV:
+                self.alu("DIV", reg_a, reg_b)
+                # Increment program counter enough to start the next program
+                self.pc += 3
+
             elif ir == self.PUSH:
+                # Decrement stack pointer
+                self.reg[7] -= 1
+                # Prevent overflow
+                self.reg[7] &= 0xff
+                # Set sp position value in ram to be registry value at position
+                self.ram[self.reg[7]] = self.reg[self.ram[self.pc+1]]
+                # Next program
                 self.pc += 2
-                continue
+
             elif ir == self.POP:
+                # Set registry value at position to be sp position value
+                self.reg[self.ram[self.pc+1]] = self.ram[self.reg[7]]
+                # Increment stack pointer
+                self.reg[7] += 1
+                # Prevent overflow
+                self.reg[7] &= 0xff
+                # Next program
                 self.pc += 2
-                continue 
+
             elif ir == self.PRN:
                 register_number = self.ram[self.pc+1]
                 print(self.reg[register_number])
+                # Increment program counter enough to start the next program
                 self.pc +=2
+
             elif ir == self.HLT:
                 running = False
+                # Increment program counter enough to start the next program
                 self.pc +=1
+
             elif ir == 0:
+                # Skip any program with a byte identifier of 0
                 continue
+
             else:
                 print(f'Unkown instruction {ir} at address {self.pc}')
+                # Exit the program
                 sys.exit(1)
